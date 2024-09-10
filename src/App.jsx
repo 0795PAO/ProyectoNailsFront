dd131d39014ba6c58b48668d5f5e3a6fdb1933d5
0795PAO <paofran0795@gmail.com>
2024-09-11 01:18:44 +0200

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import Button from "./components/button/Button"
-import Layaut from "./components/layaut/Layaut"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Button from "./components/button/Button";
+import Layaut from "./components/layaut/Layaut";
 import Navbar from "./components/navbar/Navbar";
 
-
 function App() {
-
   return (
     <>
-      <div className="bg-secondary">
-        <Navbar></Navbar>
+      <div 
+        className="bg-secondary min-h-screen flex flex-col bg-no-repeat bg-center" 
+        style={{ 
+          backgroundImage: "url('/public/imagenes/logo.png')", // Ruta de la imagen de fondo
+          backgroundSize: "contain", // Asegura que el logo se muestre completo
+          backgroundColor: "pink" // Agrega un color de fondo para que el logo sea visible
+        }}
+      >
+        <Navbar />
         <Layaut>
-          <Button name="REGISTRO" />
-          <h1>SOBRE NOSOTROS</h1>
-          <P> Ubicado convenientemente en Aurora, Colorado, 80015, America Nails & Spa es uno de los mejores salones de esta área. America Nails & Spa ofrece servicios de cuidado de uñas y tratamientos de spa de primer nivel para satisfacer sus necesidades de realzar la belleza natural y refrescar su día.mi nueva linea</P>
-
-          <P>Nuestro salón se enorgullece de brindarles a nuestros valiosos clientes todos los buenos servicios y productos y materiales de la más alta calidad. Puede encontrar todos los servicios relacionados con las uñas, desde manicura y pedicura hasta uñas acrílicas. En America Nails & Spa, nos enorgullecemos de brindarle todos los buenos productos y servicios calificados para ayudarlo a rejuvenecer su belleza. Siempre se sentirá amable y bienvenido por nuestro personal apasionado que comprende lo que desea.</P>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/"></Route>
+            </Routes>
+          </BrowserRouter>
         </Layaut>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,32 @@
-import React from "react";
-import Button from "../button/Button";
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const btnLink = 'block py-2 text-black hover:text-accent cursor-pointer mr-4';
     return (
-        <>
-        <button>ACERCA DE</button>
-        <button>SERVICIOS</button>
-        <button>GALERIA</button>
-        <button>CONTACTO</button>
-        <button>RESERVA ONLINE</button>
-        </>
-       
-    )
+        <header className="w-full text-gray-600 body-font bg-white shadow-md">
+            <div className="w-full flex flex-wrap p-5 flex-col md:flex-row items-center justify-between px-4">
+                <a href="/" className="flex items-center mb-4 md:mb-0">
+                    <img src="/public/imagenes/logo.png" alt="Rashel Nails & Spa Logo" className="w-10 h-10" />
+                    <span className="ml-3 text-xl font-semibold text-black">Rashel Nails & Spa</span>
+                </a>
+                <button className="inline-flex items-center md:hidden text-black" onClick={() => setIsOpen(!isOpen)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24" strokeLinecap="round">
+                        <path d="M4 6h16M4 12h16m-7 6h7"></path>
+                    </svg>
+                </button>
+                <nav className={`md:flex md:flex-wrap items-center text-base justify-center ${isOpen ? 'block' : 'hidden'} md:block`}>
+                    <a className={btnLink}>HOME</a>
+                    <a className={btnLink}>SERVICIOS</a>
+                    <a className={btnLink}>GALERIA</a>
+                    <a className={btnLink}>CONTACTO</a>
+                    <a className={btnLink}>OFERTA DEL MES</a>
+                    <a className={btnLink}>RESERVAR</a>
+                    <a className={btnLink}>SUSCRIBIRSE</a>
+                </nav>
+            </div>
+        </header>
+    );
 }
 
-export default Navbar
+export default Navbar;
