9f073c0cb321632768f3dca1bce8728b6d55e454
0795PAO <paofran0795@gmail.com>
2024-09-10 23:02:02 +0200

    creacion componente navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Button from "./components/button/Button"
 import Layaut from "./components/layaut/Layaut"
+import Navbar from "./components/navbar/Navbar";
 
 
 function App() {
@@ -7,12 +8,14 @@ function App() {
   return (
     <>
       <div className="bg-secondary">
-          <Button name= "ACERCA DE" />
-          <Button name= "SERVICIOS" />
-          <Button name= "GALERIA" />
-          <Button name= "CONTACTO" />
-          <Button name= "RESERVA ONLINE" />
-          <Layaut/>
+        <Navbar></Navbar>
+        <Layaut>
+          <Button name="REGISTRO" />
+          <h1>SOBRE NOSOTROS</h1>
+          <P> Ubicado convenientemente en Aurora, Colorado, 80015, America Nails & Spa es uno de los mejores salones de esta área. America Nails & Spa ofrece servicios de cuidado de uñas y tratamientos de spa de primer nivel para satisfacer sus necesidades de realzar la belleza natural y refrescar su día.mi nueva linea</P>
+
+          <P>Nuestro salón se enorgullece de brindarles a nuestros valiosos clientes todos los buenos servicios y productos y materiales de la más alta calidad. Puede encontrar todos los servicios relacionados con las uñas, desde manicura y pedicura hasta uñas acrílicas. En America Nails & Spa, nos enorgullecemos de brindarle todos los buenos productos y servicios calificados para ayudarlo a rejuvenecer su belleza. Siempre se sentirá amable y bienvenido por nuestro personal apasionado que comprende lo que desea.</P>
+        </Layaut>
       </div>
     </>
   )
